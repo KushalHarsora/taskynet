32439bd3fc7b0c944f2d9cd035615f7dc80ec1a6
Kushal Harsora <kushalharsora13@gmail.com>
2025-04-13 18:53:06 +0530

    fixed routing issue in vercel

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -9,7 +9,7 @@ const App = () => {
     <React.Fragment>
       <BrowserRouter>
         <Routes>
-        <Route path="/" element={<Home />} />
+          <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
         </Routes>
